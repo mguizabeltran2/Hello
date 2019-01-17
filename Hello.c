f2a8f4cc8a70a04e42da49158cac83801f4daf63
mguizabeltran2 <46728437+mguizabeltran2@users.noreply.github.com>
2019-01-17 14:51:09 -0600

    Initial commit

---
+++ Hello.c
@@ -0,0 +1,15 @@
+/**
+  *Author: Maria Guiza Beltran
+  *Date: 2019/17/01
+  *
+  *A simple hello world program in C
+  *
+  */
+  #include<stdlib.h>
+  #include<stdio.h>
+
+  int main(int argc, char **argv) {
+    printf("Maria!\n");
+
+    return 0;
+  }
